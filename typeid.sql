b5a74aae8efeed23fc9db31a85b2a52c57274330
Arkam Fahri <92285161+driftdev@users.noreply.github.com>
2024-09-16 21:18:04 +0530

    chore: cleaned typed sql

--- typeid.sql
+++ typeid.sql
@@ -1,109 +1,112 @@
-create extension if not exists pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-
-create or replace function gen_ulid()
-	returns text -- A randomly generated ULID String
-as
+CREATE OR REPLACE FUNCTION gen_ulid()
+    RETURNS text
+AS
 $$
-declare
+DECLARE
     -- Crockford's Base32
-	encoding  bytea = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
-	timestamp bytea = E'\\000\\000\\000\\000\\000\\000';
-	output    text  = '';
-	unix_time bigint;
-	ulid      bytea;
-begin
-	unix_time = (extract(epoch from clock_timestamp()) * 1000)::bigint;
-	timestamp = set_byte(timestamp, 0, (unix_time >> 40)::bit(8)::integer);
-	timestamp = set_byte(timestamp, 1, (unix_time >> 32)::bit(8)::integer);
-	timestamp = set_byte(timestamp, 2, (unix_time >> 24)::bit(8)::integer);
-	timestamp = set_byte(timestamp, 3, (unix_time >> 16)::bit(8)::integer);
-	timestamp = set_byte(timestamp, 4, (unix_time >> 8)::bit(8)::integer);
-	timestamp = set_byte(timestamp, 5, unix_time::bit(8)::integer);
+    encoding  bytea = '0123456789ABCDEFGHJKMNPQRSTVWXYZ';
+    timestamp bytea = E'\\000\\000\\000\\000\\000\\000';
+    output    text  = '';
+    unix_time bigint;
+    ulid      bytea;
+BEGIN
+    unix_time = (extract(epoch from clock_timestamp()) * 1000)::bigint;
+    timestamp = set_byte(timestamp, 0, (unix_time >> 40)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 1, (unix_time >> 32)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 2, (unix_time >> 24)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 3, (unix_time >> 16)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 4, (unix_time >> 8)::bit(8)::integer);
+    timestamp = set_byte(timestamp, 5, unix_time::bit(8)::integer);
 
-	-- 10 entropy bytes
-	ulid = timestamp || gen_random_bytes(10);
+    -- 10 entropy bytes
+    ulid = timestamp || gen_random_bytes(10);
 
-	-- Encode the timestamp
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 0) & 224) >> 5));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 0) & 31)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 1) & 248) >> 3));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 1) & 7) << 2) | ((get_byte(ulid, 2) & 192) >> 6)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 2) & 62) >> 1));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 2) & 1) << 4) | ((get_byte(ulid, 3) & 240) >> 4)));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 3) & 15) << 1) | ((get_byte(ulid, 4) & 128) >> 7)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 4) & 124) >> 2));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 4) & 3) << 3) | ((get_byte(ulid, 5) & 224) >> 5)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 5) & 31)));
+    -- Encode the timestamp
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 0) & 224) >> 5));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 0) & 31)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 1) & 248) >> 3));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 1) & 7) << 2) | ((get_byte(ulid, 2) & 192) >> 6)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 2) & 62) >> 1));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 2) & 1) << 4) | ((get_byte(ulid, 3) & 240) >> 4)));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 3) & 15) << 1) | ((get_byte(ulid, 4) & 128) >> 7)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 4) & 124) >> 2));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 4) & 3) << 3) | ((get_byte(ulid, 5) & 224) >> 5)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 5) & 31)));
 
-  -- Encode the entropy
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 6) & 248) >> 3));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 6) & 7) << 2) | ((get_byte(ulid, 7) & 192) >> 6)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 7) & 62) >> 1));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 7) & 1) << 4) | ((get_byte(ulid, 8) & 240) >> 4)));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 8) & 15) << 1) | ((get_byte(ulid, 9) & 128) >> 7)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 9) & 124) >> 2));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 9) & 3) << 3) | ((get_byte(ulid, 10) & 224) >> 5)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 10) & 31)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 11) & 248) >> 3));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 11) & 7) << 2) | ((get_byte(ulid, 12) & 192) >> 6)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 12) & 62) >> 1));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 12) & 1) << 4) | ((get_byte(ulid, 13) & 240) >> 4)));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 13) & 15) << 1) | ((get_byte(ulid, 14) & 128) >> 7)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 14) & 124) >> 2));
-	output = output || chr(get_byte(encoding, ((get_byte(ulid, 14) & 3) << 3) | ((get_byte(ulid, 15) & 224) >> 5)));
-	output = output || chr(get_byte(encoding, (get_byte(ulid, 15) & 31)));
+    -- Encode the entropy
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 6) & 248) >> 3));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 6) & 7) << 2) | ((get_byte(ulid, 7) & 192) >> 6)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 7) & 62) >> 1));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 7) & 1) << 4) | ((get_byte(ulid, 8) & 240) >> 4)));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 8) & 15) << 1) | ((get_byte(ulid, 9) & 128) >> 7)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 9) & 124) >> 2));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 9) & 3) << 3) | ((get_byte(ulid, 10) & 224) >> 5)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 10) & 31)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 11) & 248) >> 3));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 11) & 7) << 2) | ((get_byte(ulid, 12) & 192) >> 6)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 12) & 62) >> 1));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 12) & 1) << 4) | ((get_byte(ulid, 13) & 240) >> 4)));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 13) & 15) << 1) | ((get_byte(ulid, 14) & 128) >> 7)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 14) & 124) >> 2));
+    output = output || chr(get_byte(encoding, ((get_byte(ulid, 14) & 3) << 3) | ((get_byte(ulid, 15) & 224) >> 5)));
+    output = output || chr(get_byte(encoding, (get_byte(ulid, 15) & 31)));
 
-	return output;
-end
+    RETURN output;
+END
 $$
-language plpgsql volatile;
+    LANGUAGE plpgsql
+    VOLATILE;
 
-
-create or replace function check_ulid(
-	id text
+CREATE OR REPLACE FUNCTION check_ulid(
+    id text
 )
-returns boolean
-as $$
-begin
-	if id ~ '^[0-9a-zA-Z]{26}$' then
-  		return true;
-	else
-  		return false;
-	end if;
-end
+    RETURNS boolean
+AS
 $$
-language plpgsql immutable;
+BEGIN
+    IF id ~ '^[0-9a-zA-Z]{26}$' THEN
+        RETURN true;
+    ELSE
+        RETURN false;
+    END IF;
+END
+$$
+    LANGUAGE plpgsql
+    IMMUTABLE;
 
+CREATE OR REPLACE FUNCTION gen_typeid(
+    prefix text
+)
+    RETURNS text
+AS
+$$
+BEGIN
+    IF (prefix IS NULL) OR (length(prefix) = 0) OR NOT (prefix ~ '^[a-z]+(_[a-z]+)*$') THEN
+        RAISE EXCEPTION 'typeid prefix must not be null or empty, must start with a lowercase letter, consist of lowercase letters and underscores, and cannot start with an underscore or end with an underscore';
+    END IF;
 
-create or replace function gen_typeid(
-	prefix text
-)
-returns text
-as $$
-begin
-	if (prefix is null) or (length(prefix) = 0) or not (prefix ~ '^[a-z]+(_[a-z]+)*$') then
-    	raise exception 'typeid prefix must not be null or empty, must start with a lowercase letter, consist of lowercase letters and underscores, and cannot start with an underscore or end with an underscore';
-  	end if;
- 
-	return (prefix || '_' || gen_ulid());
-end
+    RETURN (prefix || '_' || gen_ulid());
+END
 $$
-language plpgsql volatile;
-    
-    
-create or replace function check_typeid(
-	id text,
-  	prefix text
+    LANGUAGE plpgsql
+    IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION check_typeid(
+    id text,
+    prefix text
 )
-returns boolean
-as $$
-begin
-	if id ~ ('^' || prefix || '_[0-9a-zA-Z]{26}$') then
-  		return true;
-	else
-  		return false;
-	end if;
-end
+    RETURNS boolean
+AS
 $$
-language plpgsql immutable;
+BEGIN
+    IF id ~ ('^' || prefix || '_[0-9a-zA-Z]{26}$') THEN
+        RETURN true;
+    ELSE
+        RETURN false;
+    END IF;
+END
+$$
+    LANGUAGE plpgsql
+    IMMUTABLE;
